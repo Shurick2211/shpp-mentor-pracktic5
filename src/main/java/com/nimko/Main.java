ce6242a0e2b44fb077e35ffc8fe057e60596c8d8
sn <shurick2211@gmail.com>
2022-12-06 17:22:59 +0200

    start

---
+++ src/main/java/com/nimko/Main.java
@@ -0,0 +1,11 @@
+package com.nimko;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        log.info("Hi!");
+    }
+}
